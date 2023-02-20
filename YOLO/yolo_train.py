878d77654643135871e3b62d1ba4481ac5cbbabc
Dıjvar Ekinci <80560833+dijvar@users.noreply.github.com>
2023-02-20 17:27:03 +0300

    gpu da çalisir hale geldi

--- YOLO/yolo_train.py
+++ YOLO/yolo_train.py
@@ -1,12 +1,23 @@
 from ultralytics import YOLO
 import torch
 
-print('cuda_avail:', torch.cuda.is_available())
-print('cuda_device:', torch.cuda.device_count())
 
-# Load a model
-model = YOLO("yolov8s.yaml")  # build a new model from scratch
-#model = YOLO("yolov8s.pt")  # load a pretrained model (recommended for training)
+if __name__ == '__main__':
+    print('cuda_avail:', torch.cuda.is_available())
+    print('cuda_device:', torch.cuda.device_count())
 
-# Train the model
-model.train(data="D:/orhan/Belgeler/Datasets/airborne_object_tracking/yolo_dataset/custom.yaml", epochs=2, imgsz=640)
+    # Load a model
+    model = YOLO("yolov8s.yaml")  # build a new model from scratch
+    #model = YOLO("yolov8s.pt")  # load a pretrained model (recommended for training)
+
+    # Train the model
+    data = "D:/orhan/Belgeler/Datasets/airborne_object_tracking/yolo_dataset/custom.yaml"
+
+    results = model.train(
+    data=data,
+    imgsz=640,
+    epochs=10,
+    batch=4,
+    name='yolov8s_custom')
+
+    #model.train(data=data, epochs=2, imgsz=640)
